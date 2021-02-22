cacb1efdeb3d3423d5b9ef215735d469ce2fede7
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-22 12:54:10 +0200

    add LoginForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { useState, useEffect } from 'react'
 import BlogList from './components/BlogList'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
 	const [blogs, setBlogs] = useState([])
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [user, setUser] = useState(null)
 
 	useEffect(() => {
 		blogService.getAll().then(blogs => setBlogs(blogs))
---
+++ src/components/LoginForm.js
@@ -0,0 +1,40 @@
+const LoginForm = ({
+	username,
+	password,
+	setUsername,
+	setPassword,
+	handleLogin,
+}) => {
+	return (
+		<div>
+			<h2>log in to the app</h2>
+			<form onSubmit={handleLogin}>
+				<div>
+					username:
+					<input
+						name='Username'
+						type='text'
+						value={username}
+						onChange={e => {
+							setUsername(e.target.value)
+						}}
+					/>
+				</div>
+				<div>
+					password:
+					<input
+						name='Password'
+						type='text'
+						value={password}
+						onChange={e => {
+							setPassword(e.target.value)
+						}}
+					/>
+				</div>
+				<button type='submit'>login</button>
+			</form>
+		</div>
+	)
+}
+
+export default LoginForm
