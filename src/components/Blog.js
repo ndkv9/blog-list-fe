d525844c151549676a89dbc914f15240bcaf870b
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-23 19:29:19 +0200

    implement show and hide blog content

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 const Blog = ({ blog }) => {
+	const [isFull, setIsFull] = useState(false)
+
 	const blogStyle = {
 		paddingTop: 10,
 		paddingLeft: 2,
@@ -7,10 +11,25 @@ const Blog = ({ blog }) => {
 		marginBottom: 15,
 	}
 
+	const display = { display: isFull ? '' : 'none' }
+
+	const toggleDisplay = () => {
+		setIsFull(!isFull)
+	}
+
 	return (
 		<div style={blogStyle}>
-			{blog.title} {blog.author}
-			<button>view</button>
+			<div>
+				{blog.title} {blog.author}
+				<button onClick={toggleDisplay}>{isFull ? 'hide' : 'view'}</button>
+			</div>
+			<div style={display}>
+				<div>{blog.url}</div>
+				<div>
+					likes:{blog.likes} <button>likes</button>
+				</div>
+				<div>{blog.user.name}</div>
+			</div>
 		</div>
 	)
 }
